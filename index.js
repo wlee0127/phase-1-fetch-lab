7a0dff2c488d12bc3aaea8c7478b2c5c1604ffcf
william lee <will.lee.npc@gmail.com>
2023-10-10 00:57:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(books => {renderBooks(books)})  
 }
-
+/*
+async function fetchBooks() {
+  let books =[];
+  const response = await fetch("https://anapioficeandfire.com/api/books");
+  const data = await response.json();
+  extractName(data,books);
+  renderBooks(books);
+}
+*/
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -11,6 +21,12 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
+function extractName(data,books){
+  for (const element of data){
+    books.push(element.name);
+  }
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
